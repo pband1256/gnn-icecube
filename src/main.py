7111f33763109d5f124e8b43591f01bdd41d2849
Hieu Le <pband1256@gmail.com>
2026-03-05 19:56:04 -0500

    Add training loop with weighted BCE loss, ROC evaluation, and checkpoint management

---
+++ src/main.py
@@ -0,0 +1,243 @@
+import os
+import time
+import logging
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from sklearn.metrics import roc_auc_score
+
+import utils
+import model
+from data_handler import construct_loader
+
+#####################
+#     CONSTANTS     #
+#####################
+TEST_NAME='Test'
+
+#######################
+#     EXPERIMENT      #
+#######################
+
+def train_one_epoch(net,
+                    criterion,
+                    optimizer,
+                    args,
+                    experiment_dir,
+                    train_loader):
+  net.train()
+  nb_train = len(train_loader) * args.batch_size
+  epoch_loss = 0
+  pred_y = np.zeros((nb_train))
+  true_y = np.zeros((nb_train))
+  weights = np.zeros((nb_train))
+  for i, batch in enumerate(train_loader):
+    X, y, w, adj_mask, batch_nb_nodes, _, _ = batch
+    optimizer.zero_grad()
+    out = net(X, adj_mask, batch_nb_nodes)
+    loss = criterion(out, y, w)
+    loss.backward()
+    optimizer.step()
+
+    beg =     i * args.batch_size
+    end = (i+1) * args.batch_size
+    pred_y[beg:end]  = out.data.cpu().numpy()
+    true_y[beg:end]  = y.data.cpu().numpy()
+    weights[beg:end] = w.data.cpu().numpy()
+
+    epoch_loss += loss.item() 
+    # Print running loss about 10 times during each epoch
+    if (((i+1) % (len(train_loader)//10)) == 0):
+      nb_proc = (i+1)*args.batch_size
+      logging.info("  {:5d}: {:.9f}".format(nb_proc, epoch_loss/nb_proc))
+
+  tpr, roc = utils.score_plot_preds(true_y, pred_y, weights,
+                                      experiment_dir, 'train', args.eval_tpr)
+  epoch_loss /= nb_train
+  logging.info("Train: loss {:>.3E} -- AUC {:>.3E} -- TPR {:>.3e}".format(
+                                                      epoch_loss, roc, tpr))
+  return (tpr, roc, epoch_loss)
+
+
+def train(
+          net,
+          criterion,
+          args, 
+          experiment_dir, 
+          train_loader, 
+          valid_loader
+          ):
+  optimizer = torch.optim.Adamax(net.parameters(), lr=args.lrate)
+  scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max')
+  # Nb epochs completed tracked in case training interrupted
+  for i in range(args.nb_epochs_complete, args.nb_epoch):
+    # Update learning rate in optimizer
+    t0 = time.time()
+    logging.info("\nEpoch {}".format(i+1))
+    logging.info("Learning rate: {0:.3g}".format(args.lrate))
+    
+    train_stats = train_one_epoch(net,
+                                  criterion,
+                                  optimizer,
+                                  args,
+                                  experiment_dir,
+                                  train_loader)
+    val_stats = evaluate(net, criterion, experiment_dir, args,
+                            valid_loader, 'Valid')
+                                
+    utils.track_epoch_stats(i, args.lrate, 0, train_stats, val_stats, experiment_dir)
+
+    # Update learning rate, remaining nb epochs to train
+    scheduler.step(val_stats[0])
+    args.lrate = optimizer.param_groups[0]['lr']
+    args.nb_epochs_complete += 1
+
+    # Track best model performance
+    if (val_stats[0] > args.best_tpr):
+      logging.warning("Best performance on valid set.")
+      args.best_tpr = float(val_stats[0])
+      utils.update_best_plots(experiment_dir)
+      utils.save_best_model(experiment_dir, net)
+      utils.save_best_scores(i, val_stats[2], val_stats[0], val_stats[1], experiment_dir)
+
+    utils.save_epoch_model(experiment_dir, net)
+    utils.save_args(experiment_dir, args)
+    logging.info("Epoch took {} seconds.".format(int(time.time()-t0)))
+    
+    if args.lrate < 10**-6:
+        logging.warning("Minimum learning rate reched.")
+        break
+
+  logging.warning("Training completed.")
+
+
+def evaluate(net,
+             criterion,
+             experiment_dir,
+             args,
+             valid_loader,
+             plot_name):
+             
+    net.eval()
+    epoch_loss = 0
+    nb_batches = len(valid_loader)
+    nb_eval = nb_batches * args.batch_size
+    # Track samples by batches for scoring
+    pred_y = np.zeros((nb_eval))
+    true_y = np.zeros((nb_eval))
+    weights = np.zeros((nb_eval))
+    evt_id = []
+    f_name = []
+    logging.info("Evaluating {} {} samples.".format(nb_eval,plot_name))
+    with torch.autograd.no_grad():
+        for i, batch in enumerate(valid_loader):
+            X, y, w, adj_mask, batch_nb_nodes, evt_ids, evt_names = batch
+            out = net(X, adj_mask, batch_nb_nodes)
+            loss = criterion(out, y, w)
+            epoch_loss += loss.item() 
+            # Track predictions, truth, weights over batches
+            beg =     i * args.batch_size
+            end = (i+1) * args.batch_size
+            pred_y[beg:end] = out.data.cpu().numpy()
+            true_y[beg:end] = y.data.cpu().numpy()
+            weights[beg:end] = w.data.cpu().numpy()
+            if plot_name==TEST_NAME:
+                evt_id.extend(evt_ids)
+                f_name.extend(evt_names)
+
+            # Print running loss 2 times 
+            if (((i+1) % (nb_batches//2)) == 0):
+                nb_proc = (i+1)*args.batch_size
+                logging.info("  {:5d}: {:.9f}".format(nb_proc,
+                                                      epoch_loss/nb_proc))
+
+    # Score predictions, save plots, and log performance
+    epoch_loss /= nb_eval # Normalize loss
+    tpr, roc = utils.score_plot_preds(true_y, pred_y, weights,
+                                      experiment_dir, plot_name, args.eval_tpr)
+    logging.info("{}: loss {:>.3E} -- AUC {:>.3E} -- TPR {:>.3e}".format(
+                                      plot_name, epoch_loss, roc, tpr))
+
+    if plot_name == TEST_NAME:
+        utils.save_test_scores(nb_eval, epoch_loss, tpr, roc, experiment_dir)
+        utils.save_preds(evt_id, f_name, pred_y, experiment_dir)
+    return (tpr, roc, epoch_loss)
+
+
+def main():
+  input_dim=7
+  spatial_dims=[0,1,2]
+  args = utils.read_args()
+
+  experiment_dir = utils.get_experiment_dir(args.name, args.run)
+  utils.initialize_experiment_if_needed(experiment_dir, args.evaluate)
+  # Logger will print to stdout and logfile
+  utils.initialize_logger(experiment_dir)
+
+  # Optionally restore arguments from previous training
+  # Useful if training is interrupted
+  if not args.evaluate:
+    try:
+      args = utils.load_args(experiment_dir)
+    except:
+      args.best_tpr = 0.0
+      args.nb_epochs_complete = 0 # Track in case training interrupted
+      utils.save_args(experiment_dir, args) # Save initial args
+
+  net = utils.create_or_restore_model(
+                                    experiment_dir, 
+                                    args.nb_hidden, 
+                                    args.nb_layer,
+                                    input_dim,
+                                    spatial_dims
+                                    )
+  if torch.cuda.is_available():
+    net = net.cuda()
+    logging.warning("Training on GPU")
+    logging.info("GPU type:\n{}".format(torch.cuda.get_device_name(0)))
+  criterion = nn.functional.binary_cross_entropy
+  if not args.evaluate:
+    assert (args.train_file != None)
+    assert (args.val_file   != None)
+    train_loader = construct_loader(
+                                args.train_file,
+                                args.nb_train,
+                                args.batch_size,
+                                shuffle=True)
+    valid_loader = construct_loader(args.val_file,
+                                    args.nb_val,
+                                    args.batch_size)
+    logging.info("Training on {} samples.".format(
+                                          len(train_loader)*args.batch_size))
+    logging.info("Validate on {} samples.".format(
+                                          len(valid_loader)*args.batch_size))
+    train(
+          net,
+          criterion,
+          args,
+          experiment_dir,
+          train_loader,
+          valid_loader
+          )
+
+  # Perform evaluation over test set
+  try:
+    net = utils.load_best_model(experiment_dir)
+    logging.warning("\nBest model loaded for evaluation on test set.")
+  except:
+    logging.warning("\nCould not load best model for test set. Using current.")
+  assert (args.test_file != None)
+  test_loader = construct_loader(args.test_file,
+                                 args.nb_test,
+                                 args.batch_size)
+  test_stats = evaluate(net,
+                        criterion,
+                        experiment_dir,
+                        args,
+                        test_loader,
+                        TEST_NAME)
+
+if __name__ == "__main__":
+  main()
---
+++ src/utils.py
@@ -0,0 +1,280 @@
+import os
+import csv
+import argparse
+import logging
+import pickle
+import yaml
+import numpy as np
+
+from sklearn.metrics import roc_auc_score, roc_curve
+
+import matplotlib; matplotlib.use('Agg') # no display on clusters
+import matplotlib.pyplot as plt
+
+import torch
+import model
+
+#####################
+#     CONSTANTS     #
+#####################
+ARGS_NAME  = 'args.yml'
+MODEL_NAME = 'model.pkl'
+BEST_MODEL = 'best_model.pkl'
+STATS_CSV  = 'training_stats.csv'
+CURRENT_BASELINE = [1.44576*10**-6, 0.04302]
+
+#####################################
+#     EXPERIMENT INITIALIZATION     #
+#####################################
+
+def read_args():
+  '''
+  Parse stdin arguments
+  '''
+
+  parser = argparse.ArgumentParser(description=
+                      'Arguments for GNN model and experiment')
+  add_arg = parser.add_argument
+
+  # Experiment
+  add_arg('--name', help='Experiment reference name', required=True)
+  add_arg('--run', help='Experiment run number', default=0)
+  add_arg('--eval_tpr',help='FPR at which TPR will be evaluated', default=0.000003)
+  add_arg('--evaluate', help='Perform evaluation on test set only',action='store_true')
+
+  # Training
+  add_arg('--nb_epoch', help='Number of epochs to train', type=int, default=2)
+  add_arg('--lrate', help='Initial learning rate', type=float, default = 0.005)
+  add_arg('--batch_size', help='Size of each minibatch', type=int, default=4)
+
+  # Dataset
+  add_arg('--train_file', help='Path to train pickle file',type=str,default=None)
+  add_arg('--val_file', help='Path to val   pickle file',type=str,default=None)
+  add_arg('--test_file', help='Path to test  pickle file',type=str,default=None)
+  add_arg('--nb_train', help='Number of training samples', type=int, default=10)
+  add_arg('--nb_val', help='Number of validation samples', type=int, default=10)
+  add_arg('--nb_test', help='Number of test samples', type=int, default=10)
+
+  # Model Architecture
+  add_arg('--nb_hidden', help='Number of hidden units per layer', type=int, default=32)
+  add_arg('--nb_layer', help='Number of network grapn conv layers', type=int, default=6)
+
+
+  return parser.parse_args()
+
+def initialize_logger(experiment_dir):
+  '''
+  Logger prints to stdout and logfile
+  '''
+  logfile = os.path.join(experiment_dir, 'log.txt')
+  logging.basicConfig(filename=logfile,format='%(message)s',level=logging.INFO)
+  logging.getLogger().addHandler(logging.StreamHandler())
+
+def get_experiment_dir(experiment_name, run_number):
+  '''
+  Saves all models within a 'models' directory where the experiment is run.
+  Returns path to the specific experiment within the 'models' directory.
+  '''
+  current_dir = os.getcwd()
+  save_dir = os.path.join(current_dir, 'models')
+  if not os.path.exists(save_dir):
+    os.mkdir(save_dir) # Create models dir which will contain experiment data
+  return os.path.join(save_dir, experiment_name, str(run_number))
+
+def initialize_experiment_if_needed(model_dir, evaluate_only):
+  '''
+  Check if experiment initialized and initialize if not.
+  Perform evaluate safety check.
+  '''
+  if not os.path.exists(model_dir):
+    initialize_experiment(model_dir)
+    if evaluate_only:
+      logging.warning("EVALUATING ON UNTRAINED NETWORK")
+
+def initialize_experiment(experiment_dir):
+  '''
+  Create experiment directory and initiate csv where epoch info will be stored.
+  '''
+  print("Initializing experiment.")
+  os.makedirs(experiment_dir)
+  csv_path = os.path.join(experiment_dir, STATS_CSV)
+  with open(csv_path, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Epoch', 'lrate', 'train_tpr', 'train_roc', 'train_loss', 'val_tpr', 'val_roc', 'val_loss', 'running_loss'])
+
+
+###########################
+#     MODEL UTILITIES     #
+###########################
+def create_or_restore_model(
+                            experiment_dir,
+                            nb_hidden,
+                            nb_layer,
+                            input_dim,
+                            spat_dims
+                            ):
+  '''
+  Checks if model exists and creates it if not.
+  Returns model.
+  '''
+  model_file = os.path.join(experiment_dir, MODEL_NAME)
+  if os.path.exists(model_file):
+    logging.warning("Loading model...")
+    m = load_model(model_file)
+    logging.warning("Model restored.")
+  else:
+    logging.warning("Creating new model:")
+    m = model.GNN(nb_hidden, nb_layer, input_dim, spat_dims)
+    logging.info(m)
+    save_model(m, model_file)
+    logging.warning("Initial model saved.")
+  return m
+
+def load_model(model_file):
+  '''
+  Load torch model.
+  '''
+  m = torch.load(model_file)
+  return m
+
+def load_best_model(experiment_dir):
+  '''
+  Load the model which performed best in training.
+  '''
+  best_model_path = os.path.join(experiment_dir, BEST_MODEL)
+  return load_model(best_model_path)
+
+def save_model(m, model_file):
+  '''
+  Save torch model.
+  '''
+  torch.save(m, model_file)
+
+def save_best_model(experiment_dir, net):
+  '''
+  Called if current model performs best.
+  '''
+  model_path = os.path.join(experiment_dir, BEST_MODEL)
+  save_model(net, model_path)
+  logging.warning("Best model saved.")
+
+def save_epoch_model(experiment_dir, net):
+  '''
+  Optionally called after each epoch to save current model.
+  '''
+  model_path = os.path.join(experiment_dir, MODEL_NAME)
+  save_model(net, model_path)
+
+
+def load_args(experiment_dir):
+  '''
+  Restore experiment arguments
+  args contain e.g. nb_epochs_complete, lrate
+  '''
+  args_path = os.path.join(experiment_dir, ARGS_NAME)
+  with open(args_path, 'r') as argfile:
+    args = yaml.load(argfile, Loader=yaml.SafeLoader)
+  logging.warning("Model arguments restored.")
+  return args
+
+def save_args(experiment_dir, args):
+  '''
+  Save experiment arguments.
+  args contain e.g. nb_epochs_complete, lrate
+  '''
+  args_path = os.path.join(experiment_dir, ARGS_NAME)
+  with open(args_path, 'w') as argfile:
+    yaml.dump(args, argfile, default_flow_style=False)
+
+######################
+#     EVALUATION     #
+######################
+def score_plot_preds(true_y, pred_y, weights, experiment_dir, plot_name, f=0.5):
+  '''
+  Compute and return weighted ROC AUC scores, TPR at given f (FPR).
+  Plot ROC curves and save plots.
+  '''
+  roc_score = roc_auc_score(true_y, pred_y, sample_weight=weights)
+  fprs, tprs, thresholds = roc_curve(true_y, pred_y, sample_weight=weights)
+  # Compute TPR at specified f
+  tpr = 0.0
+  for i, fpr in enumerate(fprs):
+    if fpr > f:
+      break
+    tpr = tprs[i]
+
+  # Plot ROC AUC curve
+  plot_roc_curve(fprs, tprs,  experiment_dir, plot_name, [f, tpr])
+
+  return tpr, roc_score
+
+def plot_roc_curve(fprs, tprs, experiment_dir, plot_name, performance):
+  '''
+  Plot and save one ROC curve.
+  '''
+  # Plot
+  plt.clf()
+  plt.plot(fprs,tprs) #plt.semilogx(fprs, tprs)
+  # Zooms
+  plt.xlim([10**-7,1.0])
+  plt.ylim([0,1.0])
+  # Style
+  plt.xlabel("False Positive Rate (1- BG rejection)")
+  plt.ylabel("True Positive Rate (Signal Efficiency)")
+  plt.scatter(performance[0], performance[1], label='GNN')
+  plt.scatter(CURRENT_BASELINE[0], CURRENT_BASELINE[1], label='Baseline')
+  plt.legend()
+  plt.grid(linestyle=':')
+  #Save
+  plotfile = os.path.join(experiment_dir, '{}.png'.format(plot_name))
+  plt.savefig(plotfile)
+  plt.clf()
+
+def update_best_plots(experiment_dir):
+  '''
+  Rename .png plots to best when called.
+  '''
+  for f in os.listdir(experiment_dir):
+    # Write over old best curves
+    if f.endswith(".png") and not f.startswith("best"):
+      old_name = os.path.join(experiment_dir, f)
+      new_name = os.path.join(experiment_dir, "best_"+f)
+      os.rename(old_name, new_name)
+      
+def track_epoch_stats(epoch, lrate, train_loss, train_stats, val_stats, experiment_dir):
+  '''
+  Write loss, fpr, roc_auc information to .csv file in model directory.
+  '''
+  csv_path = os.path.join(experiment_dir, STATS_CSV)
+  with open(csv_path, 'a') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow((epoch, lrate)+train_stats+val_stats+(train_loss,))
+
+def save_preds(evt_id, f_name, pred_y, experiment_dir):
+  '''
+  Save predicted outputs for predicted event id, filename.
+  '''
+  pred_file = os.path.join(experiment_dir, 'preds.csv')
+  with open(pred_file, 'x') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['event_id', 'filename', 'prediction'])
+    for e, f, y in zip(evt_id, f_name, pred_y):
+      writer.writerow((e, f, y))
+
+def save_test_scores(nb_eval, epoch_loss, tpr, roc, experiment_dir):
+  test_scores = {'nb_eval':nb_eval,
+                 'epoch_loss':epoch_loss,
+                 'tpr':float(tpr),
+                 'roc auc':float(roc)}
+  pred_file = os.path.join(experiment_dir, 'test_scores.yml')
+  with open(pred_file, 'x') as f:
+    yaml.dump(test_scores, f, default_flow_style=False)
+
+def save_best_scores(epoch, epoch_loss, tpr, roc, experiment_dir):
+  best_scores = {'epoch':epoch,
+                 'epoch_loss':epoch_loss,
+                 'tpr':float(tpr),
+                 'roc auc':float(roc)}
+  pred_file = os.path.join(experiment_dir, 'best_scores.yml')
+  with open(pred_file, 'w') as f:
+    yaml.dump(best_scores, f, default_flow_style=False)
